635252f7b476f7f6555d4687d4ea1c892f29c05e
bolshoeuho <19pavel76@mail.ru>
2024-07-12 23:37:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1st programm
+print(9**0.5*5)
+
+# 2nd programm
+print(9.99>9.98 and 1000 != 1000.1)
+
+# 3rd programm
+print(1234//10%100+5678//10%100)
+
+# 3rd programm
+a = 1234 # int
+b = 5678 # int
+a = (a//10)%100
+b = (b//10)%100
+print (a+b)
+
+# 4th programm
+a = 13.42 # float
+b = 42.13 # float
+c = int(a)
+d = int(b)
+e = (a*100)%100
+f = (b*100)%100
+print (c == f or d == e)
