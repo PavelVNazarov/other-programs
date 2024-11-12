ad5cb7bbe6069f8aa3b2b9d9731fb72bc14caa1b
PavelVNazarov <19pavel76@mail.ru>
2024-11-12 22:50:23 +0500

    Add files via upload

---
+++ detection_site/object_detection/models.py
@@ -0,0 +1,9 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class ImageFeed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    processed_image = models.ImageField(upload_to='processed_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
